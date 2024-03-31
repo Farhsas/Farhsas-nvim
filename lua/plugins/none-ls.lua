3160c783f8822a6fcbea193460e8066765a75e42
Zian Cattoul <ziancattoul@Zians-Laptop.local>
2024-03-31 12:06:52 +0200

    Fixed none-ls issue

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,10 +4,10 @@ return {
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null.ls.builtins.formatting.stylua,
-                null.ls.builtins.formatting.prettier,
-                null.ls.builtins.formatting.black,
-                null.ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
             },
         })
         vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
